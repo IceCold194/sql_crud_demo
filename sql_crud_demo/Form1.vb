bd50e0e40a5b78838c07ae32d7efdf5cf0502eec
Ice713 <icramzilruiz@gmail.com>
2025-11-25 19:42:07 +0800

    soft delete is working

--- sql_crud_demo/Form1.vb
+++ sql_crud_demo/Form1.vb
@@ -94,7 +94,10 @@ Public Class Form1
     End Sub
 
     Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
-        Dim query As String = "DELETE FROM `crud_demo_db`.`students_tbl` WHERE (`id` = @id);"
+        ' Dim query As String = "DELETE FROM `crud_demo_db`.`students_tbl` WHERE (`id` = @id);"
+        Dim query As String = "UPDATE `crud_demo_db`.`students_tbl` 
+                                SET is_deleted = 1
+                                WHERE (`id` = @id);"
         Try
             Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
                 conn.Open()
